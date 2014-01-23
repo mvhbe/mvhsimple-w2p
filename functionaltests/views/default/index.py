396b57cf5e34ac5382733bd03378ed414cfc9ce8
Guido Van Hoof <guido.van.hoof3@telenet.be>
2014-01-23 21:19:53 +0100

    default/index : functionele test

--- functionaltests/views/default/index.py
+++ functionaltests/views/default/index.py
@@ -3,15 +3,24 @@
 
 from functionaltest import FunctionalTest, ROOT
 
+MVHURL = ROOT + "/mvh/"
+
 
 class DefaultIndexView(FunctionalTest):
 
-    def testKanHomePageZien(self):
+    def testHomePageIsBereikbaar(self):
+        """Home page is bereikbaar"""
+        response_code = self.getResponseCode(MVHURL)
+        self.assertEqual(200, response_code)
 
-        # John opens his browser and goes to the home-page of the tukker app
-        self.browser.get(ROOT + '/mvh/')
-
-        # He's looking at the homepage and sees the Heading
-        # "Visclub Moed & Volharding"
+    def testHeeftHeader(self):
+        """Hompe page heeft header"""
+        self.browser.get(MVHURL)
         body = self.browser.find_element_by_tag_name('body')
-        self.assertIn('Visclub Moed & Volharding', body.text)
+        self.assertIn('Wedstrijden deze maand', body.text)
+
+    def testGeenWedstrijdenAanwezig(self):
+        """Geen wedstrijden aanwezig"""
+        self.browser.get(MVHURL)
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Geen wedstrijden.', body.text)

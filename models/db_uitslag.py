672aff2774a3d3b44d3f4853688356257077a99e
Guido Van Hoof <guido.van.hoof3@telenet.be>
2014-05-29 20:25:45 +0200

    aantalreeksen toegevoegd aan tabel wedstrijd

--- models/db_uitslag.py
+++ models/db_uitslag.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# TODO veld plaats per reeks toevoegen + html aanpassen
+# TODO veld opmerking toevoegen + html aanpassen
+# TODO extra tabel voorzien voor de vaste plaatsen
+# TODO schema vijver toevoegen met de vaste plaatsen
+
+
 def uitslag_link(plaats, id):
     return A(plaats, _href=URL('uitslag', 'detail',args=id))
 
@@ -19,20 +25,30 @@ def importUitslag(wedstrijdId, uitslagLijnen):
                           )
 
 
+def valideer_plaats():
+    return [IS_NOT_EMPTY(error_message="Plaats niet ingevuld !")]
+
 db.define_table('uitslag',
                 Field("wedstrijd", "reference wedstrijd"),
                 Field('volgorde', 'integer'),
                 Field('deelnemer', 'string', length=100, notnull=True,
                       label="Deelnemer(s)"),
-                Field('plaats', 'integer'),
+                Field('plaats1', 'integer',
+                      label="Plaats reeks 1"),
                 Field('gewicht1', 'integer',
                       label="Gewicht reeks 1"),
+                Field('plaats2', 'integer',
+                      label="Plaats reeks 2"),
                 Field('gewicht2', 'integer',
                       label="Gewicht reeks 2"),
+                Field('plaats3', 'integer',
+                      label="Plaats reeks 3"),
                 Field('gewicht3', 'integer',
                       label="Gewicht reeks 3"),
                 Field('totaal', 'integer',
                       label="Totaal gewicht"),
+                Field('opmerking', 'string', length=100,
+                      label="Opmerking"),
                 auth.signature
                 )
 
@@ -42,14 +58,21 @@ db.uitslag.wedstrijd.requires = [
 db.uitslag.deelnemer.requires = [
     IS_NOT_EMPTY(error_message="Deelnemer(s) niet ingevuld !")
 ]
-db.uitslag.plaats.requires = [
-    IS_NOT_EMPTY(error_message="Plaats niet ingevuld !")
+db.uitslag.plaats1.requires = [
+    valideer_plaats
+]
+db.uitslag.plaats2.requires = [
+    valideer_plaats
+]
+db.uitslag.plaats3.requires = [
+    valideer_plaats
 ]
 
+
 def valideer_volgorde(wedstrijd_id):
-    volgorde = db(db.uitslag.wedstrijd==wedstrijd_id)
+    #volgorde = db(db.uitslag.wedstrijd==wedstrijd_id)
     db.uitslag.volgorde.requires = [
         IS_NOT_EMPTY(error_message="Volgorde niet ingevuld !"),
-        IS_NOT_IN_DB(volgorde, 'uitslag.volgorde',
-                     error_message="Volgorde bestaat reeds !")
+        #IS_NOT_IN_DB(volgorde, 'uitslag.volgorde',
+        #             error_message="Volgorde bestaat reeds !")
     ]
